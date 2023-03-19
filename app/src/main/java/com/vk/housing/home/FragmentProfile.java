610e75c2dd8213345701309b77f84915f245225c
teja <teja.android5@gmail.com>
2023-03-19 18:13:09 +0530

    added settings

--- app/src/main/java/com/vk/housing/home/FragmentProfile.java
+++ app/src/main/java/com/vk/housing/home/FragmentProfile.java
@@ -1,5 +1,6 @@
 package com.vk.housing.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.vk.housing.R;
+import com.vk.housing.settings.SettingsActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +63,15 @@ public class FragmentProfile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        view.findViewById(R.id.tv_profile).setOnClickListener(view1 -> {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
+
+
 }
---
+++ app/src/main/java/com/vk/housing/settings/SettingsActivity.java
@@ -0,0 +1,17 @@
+package com.vk.housing.settings;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.vk.housing.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
